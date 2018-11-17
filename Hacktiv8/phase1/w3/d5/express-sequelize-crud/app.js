e9a27aae6026d9e9231ba442ff3f975edae8adfd
taqiabdulaziz <svx.annexiv@gmail.com>
2018-11-17 13:06:31 +0700

    1st commit

---
+++ Hacktiv8/phase1/w3/d5/express-sequelize-crud/app.js
@@ -0,0 +1,9 @@
+const express = require(`express`)
+const app = express()
+const ejs = require(`ejs`)
+const bodyParser = require(`body-parser`)
+
+app.listen(3000, function() {
+    console.log(`Listening to port`);
+})
+
